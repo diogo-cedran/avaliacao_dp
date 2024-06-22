e40a397df0335021f70ed6e45ffc557c95a16b02
Pedro Henrique Gomes <130695589+Pedrogom7@users.noreply.github.com>
2024-06-21 23:54:59 -0300

    Update pokemon.module.ts

--- src/module/pokemon.module.ts
+++ src/module/pokemon.module.ts
@@ -1,13 +1,2 @@
 // src/pokemon/pokemon.module.ts
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Pokemon } from './pokemon.entity';
-import { PokemonService } from './pokemon.service';
-import { PokemonController } from './pokemon.controller';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Pokemon])],
-  providers: [PokemonService],
-  controllers: [PokemonController],
-})
-export class PokemonModule {}
+//atualizado e transferido para a pasta pokemon
